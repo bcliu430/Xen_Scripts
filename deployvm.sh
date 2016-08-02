15d25c7829575ab462eeff6e7b24e8ba20267fc4
bcliu430 <lbc0430@gmail.com>
2016-08-02 08:14:02 -0400

    Add files via upload

---
+++ deployvm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "How many vms do you want to create?"
+
+read value
+
+echo "Create $value vms...."
+
+for ((i=0;i<value;i++))
+do
+    sudo xen-create-image --hostname=vm$value --memory=1024mb --vcpus=1 --bridge=xenbr0 \
+--ip=10.0.0.2 --gateway=10.0.0.1 --netmask=255.255.255.0 --size=1gb --password=a \
+--dir=/home/beichen/xen-domains --genpass=0 --install-method=debootstrap --noswap
+done
