610a7108dc383658ff9e053d6685a4e86343b2e3
bcliu430 <lbc0430@gmail.com>
2016-08-02 08:27:03 -0400

    Update deployvm.sh

--- deployvm.sh
+++ deployvm.sh
@@ -8,7 +8,7 @@ echo "Create $value vms...."
 
 for ((i=0;i<value;i++))
 do
-    sudo xen-create-image --hostname=vm$value --memory=1024mb --vcpus=1 --bridge=xenbr0 \
+    sudo xen-create-image --hostname=vm$vi --memory=1024mb --vcpus=1 --bridge=xenbr0 \
 --ip=10.0.0.2 --gateway=10.0.0.1 --netmask=255.255.255.0 --size=1gb --password=a \
 --dir=/home/beichen/xen-domains --genpass=0 --install-method=debootstrap --noswap
 done
