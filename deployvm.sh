a86976f4a55f26c4fc62f5c9345e52f946483324
bcliu430 <lbc0430@gmail.com>
2016-08-03 11:08:16 -0400

    Update deployvm.sh

--- deployvm.sh
+++ deployvm.sh
@@ -11,10 +11,20 @@ echo "Create $VM_NUM vms...."
 
 for ((i=0;i<$VM_NUM;i++))
 do
-echo "pass $i"
-let ip="$i+2"
-    sudo xen-create-image --hostname=vm$i --memory=${MEM_MB}mb --vcpus=$Vcpu --bridge=$bridge \
---ip=10.0.0.$ip --gateway=10.0.0.1 --netmask=255.255.255.0 --size=${VDSize}gb --password=a \
---dir=$dir --genpass=0 --install-method=debootstrap --noswap
+    echo "pass $i"
+    let ip="$i+2"
+        sudo xen-create-image --hostname=vm$i \
+        --memory=${MEM_MB}mb \
+        --vcpus=$Vcpu \
+        --bridge=$bridge \
+        --ip=10.0.0.$ip \
+        --gateway=10.0.0.1 \
+        --netmask=255.255.255.0 \
+        --size=${VDSize}gb \
+        --password=a \
+        --dir=$dir \
+        --genpass=0 \
+        --install-method=debootstrap \
+        --noswap
 
 done
