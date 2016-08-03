15cf96291c28882360bc23ea03b885cebfd539ab
bcliu430 <lbc0430@gmail.com>
2016-08-03 02:06:05 -0400

    Add files via upload

---
+++ delCon.mk
@@ -0,0 +1,10 @@
+#This configuration file is used to delete vm
+
+#Directory of the vms that want to delete
+Dir=~/xen-domains
+
+#set start vm
+start=1
+
+#set stop vm
+stop=5
---
+++ delCon.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+set -euo pipefail
+
+if [ ! "$#" = "1" ]; then
+    echo "Usage:$0 <config file>"
+fi
+
+source $1
+
+echo "Delete vm $start to $stop"
+
+for ((i=${start};i<${stop}+1;i++))
+do
+   echo "pass $i"
+   #stop vm
+   sudo xl destory vm$i
+   #delete image file from directory given in config
+   sudo xen-delete-image vm$i --dir=$dir
+done
