74b10afd28b5af4e850e5e144c8184aae2893d59
Pierre Olivier <polivier@vt.edu>
2016-08-04 18:47:03 -0400

    Various addition and fixes

--- config.mk
+++ config.mk
@@ -1,10 +1,12 @@
-
-# TODO
-# PREFIX=vm
-# FIRST_ID=0
+# VM naming:
+# The first vm name will be the concatennation of PREFIX + ID
+# ex: PREFIX=vm, ID=0 so the name of the first machine created  will be vm0
+# The last vm id will be computed as ID + VM_NUM -1
+PREFIX=vm
+ID=0
 
 # Directory for vm
-dir=/root/xen-domains
+XT_DIR=/root/xen-domains
 
 ###This part is used to deploy vms
 
@@ -12,17 +14,21 @@ dir=/root/xen-domains
 VM_NUM=1 
 
 # Amount of memory for the VM in MB
-memory=1024
+MEMORY=1024
 
 # Size of Virtual disk in GB
-size=1
+DISK_SIZE=1
 
 # Number of Vcpu
-Vcpu=1
+VCPUS=1
 
-# Network bridge
-bridge=xenbr0
-
+# Networking - the ip will be computed based on GATEWAY (first 3 numbers), and
+# BASE_IP_NUM. Fors example if we are creating 3 VMs, GATEWAY=10.0.0.1, and
+# BASE_IP_NUM=2, their ip will be 10.0.0.2, 10.0.0.3 and 10.0.0.4
+BRIDGE=xenbr0
+NETMASK=255.255.255.0
+GATEWAY=10.0.0.1
+BASE_IP_NUM=2
 ###
 
 
