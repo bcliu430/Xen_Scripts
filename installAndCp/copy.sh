5ec5084f849cca2a84d4160577948db013f0564a
Pierre Olivier <polivier@vt.edu>
2016-08-09 00:03:32 -0400

    Fixed copy script (still need some work but should be easy now) + changed the format in the related config file

--- installAndCp/copy.sh
+++ installAndCp/copy.sh
@@ -8,13 +8,10 @@ if [ ! "$#" = "4" ];then
 fi
 
 while IFS='' read -r line; do
-    for i in 'seq $2 $3'; do
-        # set FS to \t; will change this to other after I finish the code
-        # awk -F '\t' 'echo $0 root@$1$i $1':'' copy' $1
-        # Here I am confused about the field
-        # I want to do things like $0 root@$1$i $1 
-        # But the first $0 and the last $1 is in the config file and $1$i is the field in bash
-    done
-done < $1
+    echo $line
+    source=`echo $line | sed -n "s/\(.*\)\;\(.*\)/\1/p"`
+    target=`echo $line | sed -n "s/\(.*\)\;\(.*\)/\2/p"`
 
+    echo "$source --> $target"
 
+done < $4
