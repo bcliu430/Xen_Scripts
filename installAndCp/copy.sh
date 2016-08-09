100d9c5a4c5b203181cfb4c133ba0ea6c32473fe
root <lbc0430@gmail.com>
2016-08-09 08:32:38 -0400

    add iteration

--- installAndCp/copy.sh
+++ installAndCp/copy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -euxo pipefail
+set -euo pipefail
 
 if [ ! "$#" = "4" ];then
     echo "Usage: $0 <prefix> <start> <stop> <config file>"
@@ -11,7 +11,9 @@ while IFS='' read -r line; do
     echo $line
     source=`echo $line | sed -n "s/\(.*\)\;\(.*\)/\1/p"`
     target=`echo $line | sed -n "s/\(.*\)\;\(.*\)/\2/p"`
-
-    echo "$source --> $target"
-
+   
+    for i in `seq $2 $3`; do
+        #scp $source "root@$1$i:"$target
+        echo "$source --> "root@$1$i:"$target" 
+    done
 done < $4
