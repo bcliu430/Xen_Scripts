c0bc1a2c61b018198a1316497ec69557f4195ac3
root <lbc0430@gmail.com>
2016-09-25 11:59:59 -0400

    add result collecting files for postmark

---
+++ postmark/result_read.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -euxo pipefail
+#This script is used to run the script and gather result using postmark benchmark
+#Usage ./result_capture.sh <config>
+
+script -c 'postmark config|grep read'  -q result_read
+# script -c 'postmark config|grep write' -q result_write 
