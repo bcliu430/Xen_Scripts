7e9d5c33a5e4b63d2c236aafbea924aac3437342
root <lbc0430@gmail.com>
2016-09-27 18:00:20 -0400

    update result_read.sh

--- postmark/result_read.sh
+++ postmark/result_read.sh
@@ -4,5 +4,5 @@ set -euxo pipefail
 #This script is used to run the script and gather result using postmark benchmark
 #Usage ./result_capture.sh <config>
 
-script -c 'postmark config|grep read'  -q result_read
+postmark config|grep "megabytes read" 
 # script -c 'postmark config|grep write' -q result_write 
