c29117c45b19a82f0b8cdb3d931dfc97e65b5f8b
Pierre Olivier <polivier@vt.edu>
2016-09-28 11:21:00 -0400

    fixed postmark read error msg

--- postmark/result_read.sh
+++ postmark/result_read.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
 set -euo pipefail
 
 echo -n "" > result_read_$(hostname).dat
 
 for i in $(seq 1 5); do 
-    postmark config| grep "megabytes read" | sed 's\(\\g' |awk -F' '  ' {print $4}'  >> result_read_$(hostname).dat
+    postmark config
+    cat pm-output | grep "megabytes read" | sed 's\(\\g' | awk -F' '  ' {print $4}'  >> result_read_$(hostname).dat
 done
 
